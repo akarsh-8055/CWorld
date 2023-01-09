0a0f278f5543219cc0e52cd63367e485ce4f7822
akarsh-8055 <121494071+akarsh-8055@users.noreply.github.com>
2023-01-09 18:55:20 +0530

    Add files via upload

---
+++ PATTERN8.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n;
+clrscr();
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=4*n-1;i++)
+  {
+    for(j=1;j<=2*n-1;j++)
+      {
+	if(i<n)
+	   {
+	    (j==(n-1)+i||j==(n+1)-i)?printf("*"):printf(" ");
+	   }
+	else if(i>=n&&i<=3*n-1)
+	   {
+	    (i==n||j==1||i==3*n-1||j==2*n-1||i==n+j||i==3*n-j)?printf("*"):printf(" ");
+	   }
+	else if(i>3*n-1&&i<=4*n-1)
+	   {
+	     (i==(3*n-1)+j||i==(5*n-1)-j)?printf("*"):printf(" ");
+	   }
+      }
+       printf("\n");
+  }
+  getch();
+}
+
+
