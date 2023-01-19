ad0d7296eb6078fd53ada05d35d0fc457f4ee398
akarsh-8055 <121494071+akarsh-8055@users.noreply.github.com>
2023-01-19 17:07:21 +0530

    Add files via upload

---
+++ FLEAP.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int leap(int);
+int year,s;
+clrscr();
+printf("enter year ");
+scanf("%d",&year);
+s=leap(year);
+s==1?printf("leap year"):printf("not a leap year");
+getch();
+}
+int leap(int year)
+{
+if((year%400==0)||(year%4==0&&year%100!=0))
+  return 1;
+else
+  return 0;
+}
